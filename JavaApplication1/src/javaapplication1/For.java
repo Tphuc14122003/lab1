6421ac77f3b493bd5d6f80a27cac36f5c069a701
Tphuc14122003 <nguyenthienphuc242003@gmail.com>
2025-03-09 15:45:36 +0700

    8B for lab1

---
+++ JavaApplication1/src/javaapplication1/For.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nguye
+ */
+public class For {
+
+    public static void main(String[] args) {
+//        int rong = 7;
+//        int dai = 20;
+//        for (int i = 0; i < rong; i++) {
+//            for (int j = 0; j < dai; j++) {
+//                if (i == 0 || i == rong - 1 || j == 0 || j == dai - 1) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println("");
+//        }
+
+// Bài 2
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("nhập số dòng ");
+//        int dong = sc.nextInt();
+//        
+//        for( int i =1; i <=dong; i++) {
+//            for(int j =1; j <= i; j++) {
+//                   System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+//          Bài 3
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(" Nhập chiều cao");
+//        int h = sc.nextInt();
+//
+//        for (int i = 1; i <= h; i++) {
+//            for (int j = h - 1; j > 0; j--) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= i; k++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//      Bài4 
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(" Nhập số nguyên dương ");
+//        int n = sc.nextInt();
+//        if (n <= 1) {
+//            System.out.println("Không phải số nguyên tố");
+//        }
+//        boolean isPrime = true;
+//        for (int i = 2; i <= Math.sqrt(n); i++) {
+//            if (n % i == 0) {
+//                isPrime = false;
+//                break;
+//            }
+//        }
+//        if (isPrime) {
+//            System.out.println(n + "là số nguyên tố");
+//        } else {
+//            System.out.println(n + "Không là số nguyên tố");
+//        }
+//          bài 5 
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(" Nhập số nguyên      ");
+//        int n = sc.nextInt();
+//
+//        System.out.println(" Các số nguyên từ 1 đến " + n + "là:");
+//        for (int i = 2; i <= n; i++) {
+//            boolean isPrime = true;
+//            for (int j = 2; j <= Math.sqrt(i); j++) {
+//                if (i % j == 0) {
+//                    isPrime = false;
+//                    break;
+//                }
+//            }
+//            if (isPrime) {
+//                System.out.println(i + " ");
+//            }
+//        }
+
+//      Bài 6 
+//        System.out.println(" Bảng cửu chương");
+//        for(int i=1 ; i <=9; i++) {
+//            for (int j=1 ; j <= 9; j++) {
+//                System.out.println(i +"x" + j +"=" + (i*j) );
+//            }
+//            System.out.println("");
+//        }
+
+//      Bài 7
+//bai 8
+// Bài 9
+ Scanner sc = new Scanner(System.in);
+        System.out.println(" Nhập số nguyên");
+        int n = sc.nextInt();
+    }
+}
--- JavaApplication1/src/javaapplication1/lab3.java
+++ JavaApplication1/src/javaapplication1/lab3.java
@@ -5,12 +5,112 @@
  */
 package javaapplication1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author nguye
  */
 public class lab3 {
+
+    // bài 1
     public static void main(String[] args) {
-        
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Nhập chiều dài");
+//        double a = sc.nextDouble();
+//        System.out.println("Nhập chiều rộng");
+//        double b = sc.nextDouble();
+//
+//        double chu_vi = 2 * (a + b);
+//        double dien_tich = a * b;
+//
+//        System.out.println("chu vi là " + chu_vi);
+//        System.out.println("diện tích là " + dien_tich);
+        // bài 2
+//        Scanner sc = new Scanner(System.in);
+//          System.out.println("Nhập bán kính");
+//        double a = sc.nextDouble();
+//        
+//        double pi = 3.14159;
+//        double chu_vi = 2 *pi*a;
+//        double diện_tích = pi*a*a;
+//        
+//        System.out.println("chu vi là " + chu_vi);
+////        System.out.println("diện tích là " + diện_tích);
+
+// bài 3 
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Nhập tiền điện");
+//        double a = sc.nextDouble();
+//
+//        double giá_điện = 3.14159;
+//        double tiền_điện = a * giá_điện;
+//        System.out.println("Tiền điện là " + tiền_điện);
+// Bài 4
+//   Scanner sc = new Scanner(System.in);
+//        System.out.println("Nhập cân nặng");
+//        double a = sc.nextDouble();
+//        
+//        System.out.println("Nhập chiều cao");
+//        double b = sc.nextDouble();
+//
+//        double  bmi = a /(b*b);
+//        System.out.println("bmi là:  " + bmi);
+        // bài 5
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Nhập đấy 1 hình thang");
+//        double a = sc.nextDouble();
+//        
+//        System.out.println("Nhập đấy 2 hình thang");
+//        double b = sc.nextDouble();
+//
+//        System.out.println("Nhập chiều cao hình thang");
+//        double c = sc.nextDouble();
+//        
+//        double  diện_tích =  (a+b)*c/2;
+//        System.out.println("bmi là:  " + diện_tích);
+        //bài 6
+//          Scanner sc = new Scanner(System.in);
+//        System.out.println("Nhập 1 số ");
+//        int a = sc.nextInt();
+//        if(a %2 ==0) {
+//            System.out.println(a+ "là số chẵn");
+//        } else {
+//            System.out.println(a+ "là số lẻ");
+//        }
+        // bài 7 
+//          Scanner sc = new Scanner(System.in);
+//        System.out.println("Nhập số thứ 1 ");
+//        int a = sc.nextInt();
+//         System.out.println("Nhập số thứ 2 ");
+//        int b = sc.nextInt();
+//         System.out.println("Nhập số thứ 3 ");
+//        int c = sc.nextInt();
+//        
+//        int số_lớn_nhất = a;
+//        if (b >a) {
+//            số_lớn_nhất =b;
+//        }
+//        if( c >a) {
+//            số_lớn_nhất= c;
+//        }
+//        System.out.println("Số lớn nhất là "+ số_lớn_nhất);
+        // Bài 8
+        // Bài 9
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập số nguyên dương ");
+        int a = sc.nextInt();
+
+        if (a < 0) {
+            System.out.println("Nhập số nguyên dương");
+           
+        }
+        long giaithua = 1;
+        for (int i = 1; i <= a; i++) {
+            giaithua *= i;
+        }
+                System.out.println("giai thừa của" +a+ "là" + giaithua);
+
+        // Bài 10
     }
 }
